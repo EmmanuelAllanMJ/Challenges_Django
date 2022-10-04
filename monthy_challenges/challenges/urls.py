bb8c4521c69ca85a9aba1cfdbf116f8974027f2c
Emmanuel Allan <emmanuelallanmj@gmail.com>
2022-10-04 11:38:15 +0530

    Adding january url

---
+++ monthy_challenges/challenges/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+# To import all the fn in views.py
+from . import views
+
+# When a request reaches january execute index fn
+# Here we are pointing to the function
+urlpatterns = [
+    path("january",views.index)
+]
+
+# Now we need to config urls which should be triggered for the incoming request
--- monthy_challenges/challenges/views.py
+++ monthy_challenges/challenges/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+# The argument will be passed automatically into this function when it's executed
+# This fn will not ne executed by us, instead we will soon make django aware of this fn and django will execute this fn for us when an incoming request hits django's server and is forwarded to this fn
+# We will receive http request and return http response
+def index(request):
+    # return the response which we need to send back to the client
+    return HttpResponse("This works")
+# Now we need to tell django when should we call this fn, ir for which url request. So we create urls.py
