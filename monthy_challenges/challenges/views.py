9e2105bb9151fba09367cf2b4966f29fa86c1b25
Emmanuel Allan <emmanuelallanmj@gmail.com>
2022-10-04 14:04:15 +0530

    Added redirects and dynamic views

--- monthy_challenges/challenges/views.py
+++ monthy_challenges/challenges/views.py
@@ -1,19 +1,38 @@
 import re
+from turtle import forward
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 
 # Create your views here.
+monthly_challenges = {    
+    "january":"Eat no meat for the entire month",
+    "february": "Walk for at least 20 min everyday",
+    "march":"Learn django at least 20 min everyday",
+    "april":"april",
+    "may":"may",
+    "june":"june",
+    "july":"july",
+    "august":"august",
+    "september":"september",
+    "october":"october",
+    "november":"november",
+    "december":"december",
+}
 
 def monthly_challenge_int(request,month):
+    # Here we'll get the list
+    months = list(monthly_challenges.keys())
+    
+    if month>12:
+        return HttpResponseNotFound("Invalid month")
+    redirect_month = months[month-1]
+    return HttpResponseRedirect("/challenges/"+redirect_month)
+
     return HttpResponse(month)
 
 def monthly_challenge(request,month):
-    if month=="january":
-        challenge_text = "Eat no meat for the entire month"
-    elif month=="february":
-        challenge_text = "Walk for at least 20 min everyday"
-    elif month=="march":
-        challenge_text = "Learn django at least 20 min everyday"
-    else:
-        return HttpResponseNotFound("This month is not supported")
-    return HttpResponse(challenge_text)
+    try:
+        challenge_text = monthly_challenges[month]
+        return HttpResponse(challenge_text)
+    except:
+        return HttpResponseNotFound("This month is not supported!")
